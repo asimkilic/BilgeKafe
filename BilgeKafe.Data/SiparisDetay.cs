43edc7733ca40174d2d0fc3f06d80a1419d1361f
asimkilic <a.asim.kilic@gmail.com>
2021-09-30 15:39:28 +0300

    Data katmanı

--- BilgeKafe.Data/SiparisDetay.cs
+++ BilgeKafe.Data/SiparisDetay.cs
@@ -1,17 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace BilgeKafe.Data
 {
+    [Table("SiparisDetaylar")]
     public class SiparisDetay
     {
+        public int Id { get; set; }
+        [Required, MaxLength(100)]
         public string UrunAd { get; set; }
         public decimal BirimFiyat { get; set; }
         public int Adet { get; set; }
+
+        [NotMapped]
         public string TutarTL => $"{Tutar():N2}₺";
+
+        public int UrunId { get; set; }
+        public virtual Urun Urun { get; set; }
+        public int SiparisId { get; set; }
+        public virtual Siparis Siparis { get; set; }
 
         public decimal Tutar()
         {
